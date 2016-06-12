4e00966bf8ee6e67a116d5755490bc317ff7abfa
gilles <gillesp@gmail.com>
2016-06-12 14:35:44 +0200

    auto install packages

--- init.el
+++ init.el
@@ -1,1 +1,11 @@
-(require 'helm)
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+			 ("marmalade" . "https://marmalade-repo.org/packages/")
+			 ("melpa" . "https://melpa.milkbox.net/packages/")))
+(package-initialize)
+(unless package-archive-contents (package-refresh-contents))
+
+(dolist (package '(helm magit))
+  (unless (package-installed-p package)
+    (package-install package)))
+
+
