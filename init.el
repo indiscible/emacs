f8a4a3d8b62255b6acdf47de5596e6163144db8f
gilles <gillesp@gmail.com>
2016-06-13 00:37:31 +0200

    gtags

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
 (package-initialize)
 (unless package-archive-contents (package-refresh-contents))
 
-(dolist (package '(helm magit))
+(dolist (package '(helm helm-gtags magit))
   (unless (package-installed-p package) (package-install package)))
 
 (require 'helm)
@@ -41,3 +41,26 @@
 (global-set-key (kbd "<C-shift-f12>") 'next-error)
 ;; other
 (global-set-key (kbd "C-c g") 'magit-status)
+
+(require 'setup-helm)
+(setq
+ helm-gtags-ignore-case t
+ helm-gtags-auto-update t
+ helm-gtags-use-input-at-cursor t
+ helmm-gtags-pulse-at-cursor t
+ helm-gtags-prefix-key "\C-cg"
+ helm-gtags-suggested-key-mapping t
+ )
+(require 'helm-gtags)
+(add-hook 'dired-mode-hook 'helm-gtags-mode)
+(add-hook 'eshell-mode-hook 'helm-gtags-mode)
+(add-hook 'c-mode-hook 'helm-gtags-mode)
+(add-hook 'c++-mode-hook 'helm-gtags-mode)
+(add-hook 'asm-mode-hook 'helm-gtags-mode)
+;(define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
+
+(define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
+(define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
+(define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
+(define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
+(define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
