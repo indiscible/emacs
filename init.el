34e00efb0e048a89590385efad3eb40a910fa67f
gilles <gillesp@gmail.com>
2016-06-12 18:20:16 +0200

    helm config

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+;; Auto load packages
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
 			 ("marmalade" . "https://marmalade-repo.org/packages/")
 			 ("melpa" . "https://melpa.milkbox.net/packages/")))
@@ -5,7 +6,37 @@
 (unless package-archive-contents (package-refresh-contents))
 
 (dolist (package '(helm magit))
-  (unless (package-installed-p package)
-    (package-install package)))
+  (unless (package-installed-p package) (package-install package)))
 
+(require 'helm)
+(require 'helm-config)
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-set-key (kbd "C-c h o") 'helm-occur)
+(global-set-key (kbd "C-c h x") 'helm-register)
+(global-set-key (kbd "C-h SPC") 'helm-all-mark-rings)
+(global-set-key (kbd "C-c h g") 'helm-google-suggest)
 
+(global-unset-key (kbd "C-x c"))
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
+(define-key helm-map (kbd "C-z") 'helm-select-action)
+
+(setq helm-split-window-in-side-p t
+      helm-move-to-line-cycle-in-source t
+      helm-ff-search-library-in-sexp t
+      helm-scroll-amount 8
+      helm-ff-file-name-history-use-recentf t
+      helm-auto-resize-mode t)
+
+(helm-mode 1)
+
+;; compile stuff
+(global-set-key (kbd "<C-return>") 'recompile)
+
+;; other
+(global-set-key (kbd "C-c g") 'magit-status)
